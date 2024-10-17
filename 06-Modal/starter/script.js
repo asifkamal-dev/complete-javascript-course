3ea16760380d05f384ee67e42945f1f148e6bc6e
Asif Kamal <asif.kamal.dev@gmail.com>
2024-10-17 16:18:46 -0500

    build: Add function to close modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,9 +5,23 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 
+function closeModal () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+function openModal () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+
 console.log(btnShowModal);
 
 for (let i = 0; i < btnShowModal.length; i++) {
     console.log(btnShowModal[i].textContent);
-       
+    btnShowModal[i].addEventListener('click', openModal);
+    btnCloseModal.addEventListener('click', closeModal)
 }
+
+overlay.addEventListener('click', closeModal)
